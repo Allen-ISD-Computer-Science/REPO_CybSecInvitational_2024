9e0144e3186f9dc2654bcd16d639d642112ab4ae
BurntWaffleCake <neondenetro@gmail.com>
2023-12-27 13:40:11 -0600

    added hosting to codermerlin vapor port

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
+const path = require("path");
+
 var express = require("express");
 var app = express();
+
+const config = require(path.join(__dirname, "config.json"));
+
+console.log(config);
 
 app.get("/", function (req, res) {
   res.send("hello world");
 });
 
-var server = app.listen(8081, function () {
+var server = app.listen(Number(config.host_port), function () {
   var host = server.address().address;
   var port = server.address().port;
 
