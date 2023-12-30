8a0b3fbfcc7d94bc3eadb4ee90fc8c5d994c482c
unknown <James@Main.texas.rr.com>
2023-12-30 15:14:42 -0600

    Binary Obscurity Puzzle

---
+++ questions/Hard/Binary Obscurity/Key.sh
@@ -0,0 +1,19 @@
+# File: key_challenge.sh
+
+#!/bin/bash
+
+# Display a more challenging puzzle to the user
+echo "Welcome to the Advanced XOR Key Challenge!"
+echo "To obtain the XOR key, solve the following puzzle:"
+echo "(((2^4 + 5) * 3) / 2) + (7 % 3) - 1 = ?"
+
+# Read the user's response
+read -p "Your answer: " user_answer
+
+# Check if the user's answer is correct
+if [ "$user_answer" == "17" ]; then
+    # Provide the correct XOR key
+    echo "Congratulations! You've solved the puzzle. Here is your XOR key: 0101"
+else
+    echo "Incorrect answer. Keep trying!"
+fi
