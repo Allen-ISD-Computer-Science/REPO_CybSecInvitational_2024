7fc50ed594a9243cd80a4aa0dca23e7545120bd4
unknown <James@Main.texas.rr.com>
2023-12-30 15:42:22 -0600

    Updated Binary Obscurity

--- questions/Hard/Binary Obscurity/Key.sh
+++ questions/Hard/Binary Obscurity/Key.sh
@@ -1,19 +1,32 @@
-# File: key_challenge.sh
-
 #!/bin/bash
 
-# Display a more challenging puzzle to the user
-echo "Welcome to the Advanced XOR Key Challenge!"
-echo "To obtain the XOR key, solve the following puzzle Round to Highest Integer:"
-echo "(((2^4 + 5) * 3) / 2) + (7 % 3) - 1 = ?"
+# Display a challenging math problem to the user
+echo "Welcome to the Advanced XOR Decryption Challenge!"
+echo "Solve the following math problem to decrypt the binary flag Round to the nearest Integer: ((19 * 5) / 3) + 7 - 2 = ?"
 
 # Read the user's response
 read -p "Your answer: " user_answer
 
 # Check if the user's answer is correct
-if [ "$user_answer" == "32" ]; then
-    # Provide the correct XOR key
-    echo "Congratulations! You've solved the puzzle. Here is your XOR key: 0101"
+if [ "$user_answer" == "37" ]; then
+    # Generate the XOR key dynamically based on the answer
+    xor_key=$(echo "obase=2; $user_answer" | bc)
+    
+    # Decrypt the binary_flag.txt file with XOR key
+    encrypted_content=$(cat binary_flag.txt | tr -d ' \t\n\r')
+    
+    # Apply XOR decryption using a loop to avoid exposing the key
+    decrypted_content=""
+    for (( i=0; i<${#encrypted_content}; i++ )); do
+        decrypted_content+=${encrypted_content:i:1}
+        decrypted_content=$(echo -n "$decrypted_content" | tr "$xor_key" "\0\1")
+    done
+    
+    # Save the decrypted content to a file
+    echo -n "$decrypted_content" > decrypted_flag.txt
+    
+    echo "Congratulations! You've solved the puzzle. The binary flag has been decrypted. Check the file 'decrypted_flag.txt'."
 else
-    echo "Incorrect answer. Keep trying!"
+    echo "Incorrect answer. You get nothing."
 fi
+
