b41e805f8e80b72101d70d862d4a7b5f5ac797ba
unknown <James@Main.texas.rr.com>
2023-12-30 15:21:42 -0600

    Fixed Math Problems

--- questions/Hard/Binary Obscurity/Key.sh
+++ questions/Hard/Binary Obscurity/Key.sh
@@ -4,14 +4,14 @@
 
 # Display a more challenging puzzle to the user
 echo "Welcome to the Advanced XOR Key Challenge!"
-echo "To obtain the XOR key, solve the following puzzle:"
+echo "To obtain the XOR key, solve the following puzzle Round to Highest Integer:"
 echo "(((2^4 + 5) * 3) / 2) + (7 % 3) - 1 = ?"
 
 # Read the user's response
 read -p "Your answer: " user_answer
 
 # Check if the user's answer is correct
-if [ "$user_answer" == "17" ]; then
+if [ "$user_answer" == "32" ]; then
     # Provide the correct XOR key
     echo "Congratulations! You've solved the puzzle. Here is your XOR key: 0101"
 else
