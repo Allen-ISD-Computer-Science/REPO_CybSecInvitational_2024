a7e0d073347badcd1da958dcd06d187a59a2ea14
BurntWaffleCake <neondenetro@gmail.com>
2024-01-29 10:20:18 -0600

    added animations when updating scoreboard of public scoreboard

--- backend/public/js/public.js
+++ backend/public/js/public.js
@@ -7,28 +7,21 @@ if (location.href.includes("/vapor/soohan-cho")) {
   socket = io();
 }
 
-//<div id="template_card" class="border rounded border-left-light p-0 m-0 mb-1" style="display: none">
-{
-  /* <div class="row p-3 border-left-primary m-0">
-<div class="pr-3 card_ranking"></div>
-<div class="vr bg-secondary" style="width: 1px"></div>
-<div class="col-sm-3 card_teamid"></div>
-<div class="vr bg-secondary" style="width: 1px"></div>
-<div class="col-sm card_schoolcard_school"></div>
-<div class="vr bg-secondary" style="width: 1px"></div>
-<div class="col-sm card_division"></div>
-<div class="vr bg-secondary" style="width: 1px"></div>
-<div class="col-sm card_puzzle_points"></div>
-<div class="vr bg-secondary" style="width: 1px"></div>
-<div class="col-sm card_scenario_points"></div>
-<div class="vr bg-secondary" style="width: 1px"></div>
-<div class="col-sm card_total_points"></div>
-</div> */
-}
+var scoreboard = null;
+
 const cardHolder = document.getElementById("card_holder");
 const templateCard = document.getElementById("template_card");
-const cardHeight = templateCard.style.height;
+const cardHeight = templateCard.getBoundingClientRect().height;
 console.log(cardHeight);
+
+function getHeightAtRank(card, rank) {
+  let currentHeight = card.offsetTop - cardHolder.offsetTop;
+  let height = rank * card.offsetHeight + 4 * (rank + 1);
+  if (rank > 0) {
+    height - 5;
+  }
+  return height - currentHeight;
+}
 
 function updateUserScore(user, rank) {
   console.log(user);
@@ -52,18 +45,21 @@ function updateUserScore(user, rank) {
   const scenarioPoints = card.getElementsByClassName("card_scenario_points").item(0);
   const totalPoints = card.getElementsByClassName("card_total_points").item(0);
 
-  ranking.textContent = String(rank);
+  ranking.textContent = String(rank + 1);
   teamid.textContent = String(user.username);
   school.textContent = "Placeholder";
   switch (user.division) {
     case 0:
+      card.classList.add("border-left-light");
       division.textContent = "Silver";
       break;
     case 1:
+      card.classList.add("border-left-warning");
       division.textContent = "Gold";
       break;
     case 2:
       division.textContent = "Platinum";
+      card.classList.add("border-left-secondary");
       break;
     default:
       division.textContent = "unknown";
@@ -73,27 +69,32 @@ function updateUserScore(user, rank) {
   scenarioPoints.textContent = String(user.scenario_points);
   totalPoints.textContent = String(user.puzzle_points + user.scenario_points);
 
-  console.log(card.getBoundingClientRect().top);
-
-  // anime({
-  //   targets: `.${user.username}`,
-  //   translateX: "250px",
-  // });
+  console.log(getHeightAtRank(card, rank));
+  anime({
+    targets: `.${user.username}`,
+    translateY: `${getHeightAtRank(card, rank)}px`,
+  });
 }
 
-function updateUI(scoreboard) {
-  console.log(scoreboard);
+function updateUI() {
+  if (!scoreboard) return;
   scoreboard.forEach((user, index) => {
     updateUserScore(user, index);
   });
 }
 
+window.onresize = function () {
+  updateUI();
+};
+
 socket.on("update_event", async (data) => {
   console.log("updating");
 
   data.sort((a, b) => {
-    return a.puzzle_points + a.scenario_points - (b.puzzle_points + b.scenario_points);
+    return -(a.puzzle_points + a.scenario_points - (b.puzzle_points + b.scenario_points));
   });
 
-  updateUI(data);
+  scoreboard = data;
+
+  updateUI();
 });
